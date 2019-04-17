820735f1511650342aea021e837a9f4d357c4c6e
Adrian Samaniego <llathander@gmail.com>
2019-04-17 02:19:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,10 +20,14 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
+  array = [1, 2]
+  element = array.pop
   
 end
 
 def remove_element_from_start_of_array(array)
+  array = [1, 2]
+  element = array.unshift("wow")
   
 end
 
